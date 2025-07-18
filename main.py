f2f456ae3be95a3f7f9b62a01fe286b1e0c5b5bc
Your Mom <yourmom@example.com>
2025-07-18 23:22:41 +0300

    Implement function calls by LLM

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.schemas import schema_get_files_info
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 
 def main():
@@ -27,10 +45,20 @@ def main():
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
 
-    print(response.text)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+    else:
+        print(response.text)
 
     if verbose:
         print(f"""User prompt: {user_prompt}
